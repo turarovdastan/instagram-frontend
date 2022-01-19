403cadf7d566f76e2d8b2dd940a48679b58b6d4b
TURAROVICH <turaaa555@gmail.com>
2022-01-19 19:00:58 +0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,124 @@
+<template>
+  <v-card class="mx-auto" max-width="1000" tile>
+      <v-img height="200" src="https://c.wallhere.com/photos/47/ad/nature_landscape_mountains_mist_Moon_starry_night_moonlight_dark-104501.jpg!d"></v-img>
+      <v-row style="margin:2.5%;position:absolute; top: 130px">
+        <v-list-item>
+        <v-list-item-avatar size="100">
+            <img
+            elevation="24"
+            src="@/assets/profile.jpg"
+            alt="John"
+            >
+        </v-list-item-avatar>
+        </v-list-item>
+    </v-row>
+     <v-card style="margin-top:60px;" class="col-12">
+            <v-card-title>
+            Turarov Dastan
+            </v-card-title>
+
+            <v-card-subtitle>
+            Web developer
+            </v-card-subtitle>
+            <v-card-text>Все мы гении. Но если вы будете судить рыбу по её способности взбираться на дерево, она проживёт всю жизнь, считая себя дурой.</v-card-text>
+            <v-row justify="space-around" class=" my-5">
+                <v-btn icon>
+                    <v-icon
+                        color=" light"
+                        >
+                        mdi-phone
+                    </v-icon> 
+                </v-btn>
+                 <v-btn icon>
+                    <v-icon
+                        color=" light"
+                        >
+                        mdi-gmail
+                    </v-icon> 
+                </v-btn>
+                 <v-btn icon>
+                    <v-icon
+                        color=" light"
+                        >
+                        mdi-instagram
+                    </v-icon> 
+                </v-btn>
+            </v-row>
+    </v-card>
+
+    <v-divider></v-divider>
+
+
+    <v-card style="padding:10px 10px 30px 10px;">
+    <div class="swiper">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+       <div class="swiper-slide">
+          <img src="@/assets/s1.jpg" />
+        </div>
+        <div class="swiper-slide">
+          <img src="@/assets/s2.jpg" />
+        </div>
+        <div class="swiper-slide">
+          <img src="@/assets/s3.jpg" />
+        </div>
+        <div class="swiper-slide">
+          <img src="@/assets/s4.jpg" />
+        </div>
+        <div class="swiper-slide">
+          <img src="@/assets/s5.jpg" />
+        </div>
+        <div class="swiper-slide">
+         <video src="@/assets/miyagi.mp4" controls></video>
+       </div>
+    </div>
+    <!-- If we need pagination -->
+    <div class="swiper-pagination"></div>
+    </div>
+     <v-card-text>Каждый живет, как хочет, и расплачивается за это сам.</v-card-text>
+    </v-card>
+  </v-card>
+</template>
+
+
+
+<script>
+import Swiper from 'swiper/swiper-bundle.min.js'
+import 'swiper/swiper-bundle.min.css'
+export default {
+    mounted(){
+        const swiper = new Swiper('.swiper', {
+            grabCursor: true,
+            effect: "creative",
+                creativeEffect: {
+                prev: {
+                    shadow: true,
+                    translate: [0, 0, -400],
+                },
+                next: {
+                    translate: ["100%", 0, 0],
+                },
+            },
+            loop: true,
+
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+        });
+    }
+}
+</script>
+
+
+<style scoped>
+.swiper {
+  width: 100%;
+  height: 500px;
+}
+
+ img,video{
+      width: 100%;
+      height: 100%;
+    }
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,47 @@
+<template>
+<v-container>
+<v-img height="200" src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"></v-img>
+
+    <form class="mt-5" @submit.prevent="submit">
+        <v-text-field
+          v-model="email"
+          label="E-mail"
+          required
+          solo
+          type="email"
+        ></v-text-field>
+        <v-text-field
+          v-model="password"
+          label="password"
+          required
+          solo
+          type="password"
+        ></v-text-field>
+      <v-btn
+        class="mr-4 col-12"
+        type="submit"
+        :disabled="invalid"
+      >
+        Login
+      </v-btn>
+    </form>
+</v-container>
+</template>
+
+
+<script>
+
+
+
+  export default {
+    data: () => ({
+      email: '',
+      password:''
+    }),
+
+    methods: {
+      submit () {
+      },
+    },
+  }
+</script>
